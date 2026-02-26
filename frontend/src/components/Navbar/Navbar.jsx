6b52e54dcd031bd8e9a329e60fd3b4dc340cce8a
Mohamed Abobakr <mabobakr277@gmail.com>
2026-02-26 16:40:32 +0200

    Create Frontend Website

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src={assets.logo} alt="UniTrade Logo" className="logo" />
+        <ul className="navbar-menu">
+            <li>home</li>
+            <li>menu</li>
+            <li>mobile app</li>
+            <li>contact us</li>
+        </ul>
+        <dev className="navbar-right">
+            <img src={assets.search_icon} alt="" />
+            <dev className="navbar-search-icon">
+                <img src={assets.basket_icon} alt="" />
+                <div className="dot"></div>
+            </dev>
+            <button>sign in</button>
+        </dev>
+    </div>
+  )
+}
+
+export default Navbar
